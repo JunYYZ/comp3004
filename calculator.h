552b70a99939c7ab66a4f22e134bd38eec53cb89
Michael Palummieri <michaelpalummieri@cmail.carleton.ca>
2025-04-12 21:19:34 -0400

    added some variables and methods to calculator header.

--- calculator.h
+++ calculator.h
@@ -6,6 +6,22 @@ class calculator
 {
 public:
     calculator();
+    int calculateTotalBolus();
+    int calculateFoodBolus();
+    int calculateCorrectionBolus();
+    int calculateInsulinOnBoard();
+    int calculateImmediateBolus();
+    int calculateExtendedBolus();
+
+private:
+    int insulinOnBoard;
+    int insulinCarbRatio;
+    int correctionFactor;
+    int targetBG;
+    int currentBG;
+    int totalBolusReq;
+    int bolusDistDuration;
+    int immediateBolusFrac;
 };
 
 #endif // CALCULATOR_H
