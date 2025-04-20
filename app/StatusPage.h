6bda05580ddfb5cc5b545921de6c823085cc9404
Michael Palummieri <michaelpalummieri@cmail.carleton.ca>
2025-04-20 17:52:18 -0400

    Removed battery level from status page

--- app/StatusPage.h
+++ app/StatusPage.h
@@ -18,7 +18,6 @@ public:
     void setCurrentBG(double bg);
     void setInsulinOnBoard(double iob);
     void setBasalRate(double rate);
-    void setBatteryLevel(int percent);
 
 signals:
     void backRequested();
