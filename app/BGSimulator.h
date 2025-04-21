befb50ecb88d4549176ca4089c1ae54e99e54698
Jun <50296957+ChefBiscuit@users.noreply.github.com>
2025-04-21 15:16:00 -0400

    bg

--- app/BGSimulator.h
+++ app/BGSimulator.h
@@ -1,11 +1,32 @@
 #ifndef BGSIMULATOR_H
 #define BGSIMULATOR_H
 
+#include <QObject>
+#include "Profile.h"
 
-class BGSimulator
-{
+class BGSimulator : public QObject {
+    Q_OBJECT
+
 public:
-    BGSimulator();
+    explicit BGSimulator(QObject* parent = nullptr);
+    void setProfile(const Profile& profile);
+
+public slots:
+    void onBolusDelivered(double units, int carbs);
+    void onTick();
+
+signals:
+    void newReading(double glucose);
+
+private:
+    double currentBG = 6.0;
+    double activeInsulin = 0.0;
+    double carbEffect = 0.0;
+
+    double carbRatio = 10.0;
+    double correctionFactor = 2.0;
+
+    int currentTimeStep = 0;
 };
 
 #endif // BGSIMULATOR_H
